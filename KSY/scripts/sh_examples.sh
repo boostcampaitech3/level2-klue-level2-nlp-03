373ea2a750abdddf02bb01c6c18228c2a8736267
kimcando <kimcando94@gmail.com>
2022-03-21 10:27:46 +0000

    wandb, seed setting, arguments updated; TODO: confusion matrix

---
+++ KSY/scripts/sh_examples.sh
@@ -0,0 +1,5 @@
+WANDB_PROJECT=klue-re \
+python train.py --model_name klue/bert-base \
+                --use_wandb False \
+                --user_name KSY \
+                --exp_name test_wandb
--- KSY/train.py
+++ KSY/train.py
@@ -1,3 +1,4 @@
+# base libraries
 import pickle as pickle
 import os
 import pandas as pd
@@ -5,27 +6,46 @@ import torch
 import sklearn
 import numpy as np
 from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score
-from transformers import AutoTokenizer, AutoConfig, AutoModelForSequenceClassification, Trainer, TrainingArguments, RobertaConfig, RobertaTokenizer, RobertaForSequenceClassification, BertTokenizer
+from transformers import AutoTokenizer, AutoConfig, AutoModelForSequenceClassification, Trainer, TrainingArguments, \
+    RobertaConfig, RobertaTokenizer, RobertaForSequenceClassification, BertTokenizer
 from load_data import *
+
+# get arguments
+from arguments import get_args
+
+# additionals
+import wandb
+import random
+
+def seed_fix(seed):
+    """seed setting 함수"""
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    np.random.seed(seed)
+    random.seed(seed)
 
 
 def klue_re_micro_f1(preds, labels):
     """KLUE-RE micro f1 (except no_relation)"""
     label_list = ['no_relation', 'org:top_members/employees', 'org:members',
-       'org:product', 'per:title', 'org:alternate_names',
-       'per:employee_of', 'org:place_of_headquarters', 'per:product',
-       'org:number_of_employees/members', 'per:children',
-       'per:place_of_residence', 'per:alternate_names',
-       'per:other_family', 'per:colleagues', 'per:origin', 'per:siblings',
-       'per:spouse', 'org:founded', 'org:political/religious_affiliation',
-       'org:member_of', 'per:parents', 'org:dissolved',
-       'per:schools_attended', 'per:date_of_death', 'per:date_of_birth',
-       'per:place_of_birth', 'per:place_of_death', 'org:founded_by',
-       'per:religion']
+                  'org:product', 'per:title', 'org:alternate_names',
+                  'per:employee_of', 'org:place_of_headquarters', 'per:product',
+                  'org:number_of_employees/members', 'per:children',
+                  'per:place_of_residence', 'per:alternate_names',
+                  'per:other_family', 'per:colleagues', 'per:origin', 'per:siblings',
+                  'per:spouse', 'org:founded', 'org:political/religious_affiliation',
+                  'org:member_of', 'per:parents', 'org:dissolved',
+                  'per:schools_attended', 'per:date_of_death', 'per:date_of_birth',
+                  'per:place_of_birth', 'per:place_of_death', 'org:founded_by',
+                  'per:religion']
     no_relation_label_idx = label_list.index("no_relation")
     label_indices = list(range(len(label_list)))
     label_indices.remove(no_relation_label_idx)
     return sklearn.metrics.f1_score(labels, preds, average="micro", labels=label_indices) * 100.0
+
 
 def klue_re_auprc(probs, labels):
     """KLUE-RE AUPRC (with no_relation)"""
@@ -39,99 +59,143 @@ def klue_re_auprc(probs, labels):
         score[c] = sklearn.metrics.auc(recall, precision)
     return np.average(score) * 100.0
 
+
 def compute_metrics(pred):
-  """ validation을 위한 metrics function """
-  labels = pred.label_ids
-  preds = pred.predictions.argmax(-1)
-  probs = pred.predictions
+    """ validation을 위한 metrics function """
+    labels = pred.label_ids
+    preds = pred.predictions.argmax(-1)
+    probs = pred.predictions
 
-  # calculate accuracy using sklearn's function
-  f1 = klue_re_micro_f1(preds, labels)
-  auprc = klue_re_auprc(probs, labels)
-  acc = accuracy_score(labels, preds) # 리더보드 평가에는 포함되지 않습니다.
+    # calculate accuracy using sklearn's function
+    f1 = klue_re_micro_f1(preds, labels)
+    auprc = klue_re_auprc(probs, labels)
+    acc = accuracy_score(labels, preds)  # 리더보드 평가에는 포함되지 않습니다.
 
-  return {
-      'micro f1 score': f1,
-      'auprc' : auprc,
-      'accuracy': acc,
-  }
+    return {
+        'micro f1 score': f1,
+        'auprc': auprc,
+        'accuracy': acc,
+    }
+
 
 def label_to_num(label):
-  num_label = []
-  with open('dict_label_to_num.pkl', 'rb') as f:
-    dict_label_to_num = pickle.load(f)
-  for v in label:
-    num_label.append(dict_label_to_num[v])
-  
-  return num_label
+    num_label = []
+    with open('dict_label_to_num.pkl', 'rb') as f:
+        dict_label_to_num = pickle.load(f)
+    for v in label:
+        num_label.append(dict_label_to_num[v])
 
-def train():
-  # load model and tokenizer
-  # MODEL_NAME = "bert-base-uncased"
-  MODEL_NAME = "klue/bert-base"
-  tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+    return num_label
 
-  # load dataset
-  train_dataset = load_data("../dataset/train/train.csv")
-  # dev_dataset = load_data("../dataset/train/dev.csv") # validation용 데이터는 따로 만드셔야 합니다.
 
-  train_label = label_to_num(train_dataset['label'].values)
-  # dev_label = label_to_num(dev_dataset['label'].values)
+def train(args,reports, exp_full_name):
+    # load model and tokenizer
+    # MODEL_NAME = "bert-base-uncased"
+    MODEL_NAME = args.model_name
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 
-  # tokenizing dataset
-  tokenized_train = tokenized_dataset(train_dataset, tokenizer)
-  # tokenized_dev = tokenized_dataset(dev_dataset, tokenizer)
+    # load dataset
+    train_dataset = load_data(args.train_data_dir)
+    # dev_dataset = load_data("../dataset/train/dev.csv") # validation용 데이터는 따로 만드셔야 합니다.
 
-  # make dataset for pytorch.
-  RE_train_dataset = RE_Dataset(tokenized_train, train_label)
-  # RE_dev_dataset = RE_Dataset(tokenized_dev, dev_label)
+    train_label = label_to_num(train_dataset['label'].values)
+    # dev_label = label_to_num(dev_dataset['label'].values)
 
-  device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    # tokenizing dataset
+    tokenized_train = tokenized_dataset(train_dataset, tokenizer)
+    # tokenized_dev = tokenized_dataset(dev_dataset, tokenizer)
 
-  print(device)
-  # setting model hyperparameter
-  model_config =  AutoConfig.from_pretrained(MODEL_NAME)
-  model_config.num_labels = 30
+    # make dataset for pytorch.
+    RE_train_dataset = RE_Dataset(tokenized_train, train_label)
+    # RE_dev_dataset = RE_Dataset(tokenized_dev, dev_label)
 
-  model =  AutoModelForSequenceClassification.from_pretrained(MODEL_NAME, config=model_config)
-  print(model.config)
-  model.parameters
-  model.to(device)
-  
-  # 사용한 option 외에도 다양한 option들이 있습니다.
-  # https://huggingface.co/transformers/main_classes/trainer.html#trainingarguments 참고해주세요.
-  training_args = TrainingArguments(
-    output_dir='./results',          # output directory
-    save_total_limit=5,              # number of total save model.
-    save_steps=500,                 # model saving step.
-    num_train_epochs=20,              # total number of training epochs
-    learning_rate=5e-5,               # learning_rate
-    per_device_train_batch_size=16,  # batch size per device during training
-    per_device_eval_batch_size=16,   # batch size for evaluation
-    warmup_steps=500,                # number of warmup steps for learning rate scheduler
-    weight_decay=0.01,               # strength of weight decay
-    logging_dir='./logs',            # directory for storing logs
-    logging_steps=100,              # log saving step.
-    evaluation_strategy='steps', # evaluation strategy to adopt during training
-                                # `no`: No evaluation during training.
-                                # `steps`: Evaluate every `eval_steps`.
-                                # `epoch`: Evaluate every end of epoch.
-    eval_steps = 500,            # evaluation step.
-    load_best_model_at_end = True 
-  )
-  trainer = Trainer(
-    model=model,                         # the instantiated 🤗 Transformers model to be trained
-    args=training_args,                  # training arguments, defined above
-    train_dataset=RE_train_dataset,         # training dataset
-    eval_dataset=RE_train_dataset,             # evaluation dataset
-    compute_metrics=compute_metrics         # define metrics function
-  )
+    device = torch.device(args.device if torch.cuda.is_available() else 'cpu')
 
-  # train model
-  trainer.train()
-  model.save_pretrained('./best_model')
+    print(device)
+    # setting model hyperparameter
+    model_config = AutoConfig.from_pretrained(MODEL_NAME)
+    model_config.num_labels = args.num_labels
+
+    model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME, config=model_config)
+    print(model.config)
+    model.parameters
+    model.to(device)
+
+    # 사용한 option 외에도 다양한 option들이 있습니다.
+    # https://huggingface.co/transformers/main_classes/trainer.html#trainingarguments 참고해주세요.
+    training_args = TrainingArguments(
+        output_dir=args.output_dir,  # output directory
+        save_total_limit=args.save_total_limit,  # number of total save model.
+        save_steps=args.save_steps,  # model saving step.
+        num_train_epochs=args.epochs,  # total number of training epochs
+        learning_rate=args.lr,  # learning_rate
+        per_device_train_batch_size=args.train_bs,  # batch size per device during training
+        per_device_eval_batch_size=args.eval_bs,  # batch size for evaluation
+        warmup_steps=args.warmup_steps,  # number of warmup steps for learning rate scheduler
+        weight_decay=args.weight_decay,  # strength of weight decay
+        logging_dir=args.logging_dir,  # directory for storing logs
+        logging_steps=args.logging_steps,  # log saving step.
+        evaluation_strategy=args.eval_strategy,  # evaluation strategy to adopt during training
+        # `no`: No evaluation during training.
+        # `steps`: Evaluate every `eval_steps`.
+        # `epoch`: Evaluate every end of epoch.
+        eval_steps=args.eval_steps,  # evaluation step.
+        load_best_model_at_end=args.load_best_model_at_end,
+        # https://docs.wandb.ai/guides/integrations/huggingface
+        report_to=reports,
+        run_name = exp_full_name,
+    )
+    trainer = Trainer(
+        model=model,  # the instantiated 🤗 Transformers model to be trained
+        args=training_args,  # training arguments, defined above
+        train_dataset=RE_train_dataset,  # training dataset
+        eval_dataset=RE_train_dataset,  # evaluation dataset
+        compute_metrics=compute_metrics  # define metrics function
+    )
+
+    # train model
+    trainer.train()
+    model.save_pretrained(args.model_save_dir)
+
+def make_dirs(args):
+    # model save
+    os.makedirs(args.model_save_dir, exist_ok=True)
+    # output
+    os.makedirs(args.output_dir, exist_ok=True)
+
+
 def main():
-  train()
+    args = get_args()
+    seed_fix(args.seed)
+
+    # make directories
+    make_dirs(args)
+    # https://docs.wandb.ai/guides/integrations/huggingface
+    if args.use_wandb:
+        # entity : 우리 그룹/팀 이름
+        # project : 우리 그룹의 프로젝트 이름
+        # name : 저장되는 실행 이름 (#TODO: 아직 convention 정하지 않음)
+        reports = 'wandb'
+        exp_full_name = f'{args.user_name}_{args.model_name}_{args.lr}_{args.optimizer}_{args.loss_fn}'
+        # wandb.init(project='klue-re',
+        #            name=exp_total_name)  # nlp-03
+        # wandb.init(project='klue-re',
+        #            name=exp_total_name,
+        #            entity='kimcando')  # nlp-03
+        # wandb.config.update(args)
+        print('#######################')
+        print(f'Experiments name: {exp_full_name}')
+        print('#######################')
+    else:
+        reports = 'none'
+        exp_full_name = ''
+        print('@@@@@@@@Notice@@@@@@@@@@')
+        print('YOU ARE NOT LOGGING RESULTS NOW')
+        print('@@@@@@@@$$$$$$@@@@@@@@@@')
+
+    train(args, reports, exp_full_name)
+    # only when using notebook
+    # wandb.finish()
 
 if __name__ == '__main__':
-  main()
+    main()
