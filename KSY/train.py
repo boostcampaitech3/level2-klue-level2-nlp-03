45b894369102f4b1bce1ca51ed664a515ae93954
kimcando <kimcando94@gmail.com>
2022-03-26 06:45:31 +0000

    style: wandb notes for eval ratio

--- KSY/train.py
+++ KSY/train.py
@@ -228,7 +228,7 @@ def main():
     # 디버깅 때는 wandb 로깅 안하기 위해서
     if args.use_wandb:
         # TODO; 실험 이름 convention은 천천히 정해볼까요?
-        exp_full_name = f'{args.user_name}_{args.model_name}_{args.split_mode}_{args.lr}_{args.optimizer}_{args.loss_fn}'
+        exp_full_name = f'{args.user_name}_{args.model_name}_{args.split_mode}({args.eval_ratio})_{args.lr}_{args.optimizer}_{args.loss_fn}'
         wandb.login()
 
         # project : 우리 그룹의 프로젝트 이름
