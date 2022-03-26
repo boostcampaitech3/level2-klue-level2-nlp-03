5d2405d43f93f7c2ee6180e7f18f07a48840a316
kimcando <kimcando94@gmail.com>
2022-03-26 06:50:25 +0000

    test : 0326

--- KSY/scripts/sh_examples.sh
+++ KSY/scripts/sh_examples.sh
@@ -1,15 +1,16 @@
-python train.py --model_name monologg/koelectra-base-v3-discriminator \
-                --use_wandb True \
-                --user_name KSY \
-                --split_mode split-basic \
-                --eval_steps 100 \
-                --logging_steps 10 \
-                --load_best_model_at_end True \
-                --epochs 6 \
-                --train_bs 32 \
-                --eval_bs 250 \
-                --output_dir ./ele_results_basic \
-                --model_save_dir ./ele_best_model_basic
+#python train.py --model_name monologg/koelectra-base-v3-discriminator \
+#                --use_wandb True \
+#                --user_name KSY \
+#                --split_mode split-basic \
+#                --eval_steps 100 \
+#                --logging_steps 10 \
+#                --load_best_model_at_end True \
+#                --epochs 6 \
+#                --train_bs 32 \
+#                --eval_bs 250 \
+#                --output_dir ./ele_results_basic \
+#                --model_save_dir ./ele_best_model_basic \
+#                --eval_ratio 0.1
 
 #python train.py --model_name klue/roberta-large \
 #                --use_wandb True \
@@ -37,16 +38,16 @@ python train.py --model_name monologg/koelectra-base-v3-discriminator \
 #                --output_dir ./ro_results_dup \
 #                --model_save_dir ./ro_best_model_dup
 #
-#python train.py --model_name klue/roberta-large \
-#                --use_wandb True \
-#                --user_name KSY \
-#                --split_mode split-basic \
-#                --eval_steps 100 \
-#                --logging_steps 10 \
-#                --load_best_model_at_end True \
-#                --epochs 6 \
-#                --train_bs 32 \
-#                --eval_bs 250 \
-#                --output_dir ./ro_results_basic \
-#                --model_save_dir ./ro_best_model
+python train.py --model_name klue/roberta-large \
+                --use_wandb True \
+                --user_name KSY \
+                --split_mode split-basic \
+                --eval_steps 100 \
+                --logging_steps 10 \
+                --load_best_model_at_end True \
+                --epochs 6 \
+                --train_bs 32 \
+                --eval_bs 250 \
+                --output_dir ./ro_results_basic \
+                --model_save_dir ./ro_best_model
 
