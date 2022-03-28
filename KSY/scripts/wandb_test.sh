95fc36a694f1824ca5c04f7f7bfda852165f1ec2
kimcando <kimcando94@gmail.com>
2022-03-28 08:29:42 +0000

    test: testing

--- KSY/scripts/wandb_test.sh
+++ KSY/scripts/wandb_test.sh
@@ -6,16 +6,16 @@
 #                --epochs 1 \
 #                --train_bs 64 \
 #                --eval_steps 3
-python train_custom.py --model_name klue/roberta-large \
+python train_model.py --model_name klue/roberta-large \
                 --use_wandb True \
                 --user_name KSY \
-                --split_mode split-basic \
-		            --loss_fn focalloss \
-                --eval_steps 1 \
+                --split_mode split-dup \
+                --eval_steps 100 \
                 --logging_steps 10 \
                 --load_best_model_at_end True \
                 --epochs 6 \
-                --train_bs 1 \
+                --train_bs 32 \
                 --eval_bs 250 \
-                --output_dir ./ro_results_dup \
-                --model_save_dir ./ro_best_model_dup
+                --output_dir ./ro_results_dup_early \
+                --model_save_dir ./ro_best_model_dup_early \
+                --eval_ratio 0.05
