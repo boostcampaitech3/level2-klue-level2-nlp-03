faf474a3ca283d8c9efb4a62baa3a5a6464ff990
kimcando <kimcando94@gmail.com>
2022-03-28 08:28:56 +0000

    feat: label smoothing loss add

--- KSY/loss.py
+++ KSY/loss.py
@@ -35,6 +35,8 @@ def get_loss(args, cls_num_list):
         per_cls_weights = torch.FloatTensor(per_cls_weights).cuda('cuda')
 
         return LDAMLoss(cls_num_list, max_m=0.5, weight=per_cls_weights, s=30)
+    elif args.loss_fn =='labelsmoothingloss':
+        return LabelSmoothingLoss(classes=30, smoothing=args.smoothing)
     else:
         raise NotImplementedError
 
@@ -80,6 +82,25 @@ class LDAMLoss(nn.Module):
 
         output = torch.where(index, x_m, x)
         return F.cross_entropy(self.s * output, target, weight=self.weight)
+
+class LabelSmoothingLoss(nn.Module):
+    def __init__(self, classes=30, smoothing=0.1, dim=-1):
+        super(LabelSmoothingLoss, self).__init__()
+        self.confidence = 1.0 - smoothing
+        self.smoothing = smoothing
+        self.cls = classes
+        self.dim = dim
+        print('using LS with',self.smoothing)
+
+    def forward(self, pred, target):
+        # EDIT need
+        breakpoint()
+        pred = pred.log_softmax(dim=self.dim)
+        with torch.no_grad():
+            true_dist = torch.zeros_like(pred)
+            true_dist.fill_(self.smoothing / (self.cls - 1))
+            true_dist.scatter_(1, target.data.unsqueeze(1), self.confidence)
+        return torch.mean(torch.sum(-true_dist * pred, dim=self.dim))
 
 # class FocalLoss(nn.Module):
 #     def __init__(self, gamma=0, alpha=None, size_average=True):
