7ca36f15495e6321d2078bbcaffbc75ca02b4159
kimcando <kimcando94@gmail.com>
2022-03-29 03:06:49 +0000

    test: testing for loss

--- KSY/scripts/loss_test.sh
+++ KSY/scripts/loss_test.sh
@@ -9,8 +9,8 @@ python train_custom.py --model_name klue/roberta-large \
                 --epochs 6 \
                 --train_bs 32 \
                 --eval_bs 250 \
-                --output_dir ./ro_results_basic_ldam \
-                --model_save_dir ./ro_best_model_dup_ldam
+                --output_dir ./ro_results_basic_ls \
+                --model_save_dir ./ro_best_model_dup_ls
 
 #python train_custom.py --model_name klue/roberta-large \
 #                --use_wandb True \
