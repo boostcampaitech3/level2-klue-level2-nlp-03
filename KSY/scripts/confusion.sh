22ae77fd491ee0232af6ab233387d207f2fb3cde
kimcando <kimcando94@gmail.com>
2022-04-07 08:18:06 +0300

    refactor: soyeon folders refactoring

--- KSY/scripts/confusion.sh
+++
@@ -1,8 +0,0 @@
-python train.py --model_name klue/bert-base \
-                --use_wandb False \
-                --user_name KSY \
-                --eval_steps 0 \
-                --load_best_model_at_end True \
-                --epochs 3 \
-                --train_bs 64 \
-                --eval_bs 64
